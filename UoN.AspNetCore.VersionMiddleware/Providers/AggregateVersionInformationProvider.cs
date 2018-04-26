5ad0a16e421ce753d765706be9c0d35e02d5be14
Jonathan Couldridge <jcouldridge@outlook.com>
2018-04-26 15:20:28 +0100

    aggregate provider

---
+++ UoN.AspNetCore.VersionMiddleware/Providers/AggregateVersionInformationProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UoN.AspNetCore.VersionMiddleware.Providers
+{
+    /// <summary>
+    /// Gets version information from multiple `IVersionInformationProviders`
+    /// aggregating the results into a Dictionary.
+    /// </summary>
+    public class AggregateVersionInformationProvider : IVersionInformationProvider
+    {
+        public IDictionary<string, IVersionInformationProvider> Providers { get; set; }
+
+        /// <summary>
+        /// Create a version provider that aggregates version information from
+        /// multiple other providers.
+        /// </summary>
+        /// <param name="providers">Optional providers to initialise this Aggregate Provider</param>
+        public AggregateVersionInformationProvider(
+            IDictionary<string, IVersionInformationProvider> providers = null)
+        {
+            Providers = providers;
+        }
+
+        /// <summary>
+        /// Gets Version Information from all the `IVersionInformationProviders
+        /// </summary>
+        /// <returns></returns>
+        public async Task<object> GetVersionInformationAsync()
+            => (await Task.WhenAll(Providers.Select(async x =>
+                    new KeyValuePair<string, object>(
+                        x.Key,
+                        await x.Value.GetVersionInformationAsync()))))
+                .ToDictionary(x => x.Key, x => x.Value);
+    }
+}
