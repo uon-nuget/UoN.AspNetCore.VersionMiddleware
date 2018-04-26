ac34feda6fdbd8cced41188a964eb92471a6607e
Jonathan Couldridge <jcouldridge@outlook.com>
2018-04-26 15:45:48 +0100

    adding easy object support to the extensions

--- UoN.AspNetCore.VersionMiddleware/VersionExtensions.cs
+++ UoN.AspNetCore.VersionMiddleware/VersionExtensions.cs
@@ -37,12 +37,16 @@ namespace UoN.AspNetCore.VersionMiddleware
         /// <param name="path">The endpoint path to use.</param>
         /// <param name="provider">
         /// An optional `IVersionInformationProvider` to provide the response body.
-        /// If none is specified, `AssemblyInformationVersonProvider` is used on the Entry Assembly.</param>
+        /// 
+        /// Alternatively can be an arbitrary .NET Object to be used as version information.
+        /// 
+        /// If none is specified, `AssemblyInformationVersonProvider` is used on the Entry Assembly.
+        /// </param>
         /// <returns>The `IApplicationBuilder` instance</returns>
         public static IApplicationBuilder MapVersion(
             this IApplicationBuilder app,
             string path,
-            IVersionInformationProvider provider = null)
+            object provider = null)
         {
             if (app == null) throw new ArgumentNullException(nameof(app));
 
@@ -51,8 +55,15 @@ namespace UoN.AspNetCore.VersionMiddleware
                 // with the EntryAssembly, as per 1.0.0 behaviour
                 return app.MapVersion(path, Assembly.GetEntryAssembly());
 
+            // If we've been given an object that is NOT a provider implementation
+            // assume it is intended to be version information
+            // and use it with an ObjectProvider
+            if (!(provider is IVersionInformationProvider))
+                provider = new ObjectProvider(provider);
+
             return app.Map(path, appBuilder =>
                 appBuilder.UseMiddleware<VersionMiddleware>(provider));
+
         }
 
         /// <summary>
@@ -63,11 +74,15 @@ namespace UoN.AspNetCore.VersionMiddleware
         /// <param name="app">The `IApplicationBuilder` instance from `Startup.Configure()`.</param>
         /// <param name="provider">
         /// An optional `IVersionInformationProvider` to provide the response body.
-        /// If none is specified, `AssemblyInformationVersonProvider` is used on the Entry Assembly.</param>
+        /// 
+        /// Alternatively can be an arbitrary .NET Object to be used as version information.
+        /// 
+        /// If none is specified, `AssemblyInformationVersonProvider` is used on the Entry Assembly.
+        /// </param>
         /// <returns>The `IApplicationBuilder` instance</returns>
         public static IApplicationBuilder UseVersion(
             this IApplicationBuilder app,
-            IVersionInformationProvider provider = null)
+            object provider = null)
             => app?.MapVersion("/version", provider)
                ?? throw new ArgumentNullException(nameof(app));
     }
