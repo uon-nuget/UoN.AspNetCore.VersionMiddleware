9a3f5288f81bb89e7e5280168a39c8990d9eccf5
Jonathan Couldridge <jcouldridge@outlook.com>
2018-05-01 15:16:40 +0100

    middleware updates to reflect UoN.VersionInformation usage

--- UoN.AspNetCore.VersionMiddleware/VersionMiddleware.cs
+++ UoN.AspNetCore.VersionMiddleware/VersionMiddleware.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
-using UoN.AspNetCore.VersionMiddleware.Providers;
+using UoN.VersionInformation;
 
 namespace UoN.AspNetCore.VersionMiddleware
 {
@@ -12,23 +12,22 @@ namespace UoN.AspNetCore.VersionMiddleware
     /// </summary>
     public class VersionMiddleware
     {
-        private readonly object _provider;
+        private readonly object _versionSource;
 
         /// <summary>
         /// Construct an instance of the middleware.
         /// </summary>
         /// <param name="next">See ASP.NET Core Docs.</param>
-        /// <param name="provider">
+        /// <param name="versionSource">
         /// An object that version information can be gotten from.
-        /// Can be an Assembly to get `AssemblyInformationalVersion` from,
-        /// or an `IVersionInformationProvider` to execute,
-        /// or a regular .NET Object.
+        /// 
+        /// See UoN.VersionInformation Docs.
         /// 
         /// Defaults to Entry Assembly.
         /// </param>
-        public VersionMiddleware(RequestDelegate next, object provider = null)
+        public VersionMiddleware(RequestDelegate next, object versionSource = null)
         {
-            _provider = provider;
+            _versionSource = versionSource ?? Assembly.GetEntryAssembly();
         }
 
         public async Task Invoke(HttpContext context)
@@ -36,37 +35,17 @@ namespace UoN.AspNetCore.VersionMiddleware
             // make it clear that we're always returning json, even if it's just a string
             context.Response.ContentType = "application/json";
 
+            var service =
+                // try and get a version information service by DI
+                (VersionInformationService)
+                    context.RequestServices.GetService(
+                        typeof(VersionInformationService))
+                // else use a new one with default options
+                ?? new VersionInformationService();
+
             await context.Response.WriteAsync(
                 JsonConvert.SerializeObject(
-                    GetObjectFromVersionSourceAsync(_provider)));
-        }
-
-        /// <summary>
-        /// Used to get Version Information objects from the built in providers
-        /// based on arbitrary object types.
-        /// </summary>
-        /// <param name="source">The version information source.</param>
-        /// <returns>An object containing the actual version information.</returns>
-        public static async Task<object> GetObjectFromVersionSourceAsync(object source)
-        {
-            // switch on what we've been passed
-            // to choose appropriate shortcut behaviours
-            switch (source ?? Assembly.GetEntryAssembly()) // default to 1.0.0 behaviour
-            {
-                // if we've been passed an assembly, use the assembly provider
-                case Assembly a:
-                    return await new AssemblyInformationalVersionProvider(a).GetVersionInformationAsync();
-
-                // if we've been passed a provider, use it directly
-                case IVersionInformationProvider p:
-                    return await p.GetVersionInformationAsync();
-
-                // If we've been given an object that doesn't match the above
-                // assume it is intended to be version information
-                // and use it as is
-                default:
-                    return source;
-            }
+                    service.FromSourceAsync(_versionSource)));
         }
     }
 }
